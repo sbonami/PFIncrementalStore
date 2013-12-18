7858185cce12cc41c1467dee4bcc92b0dc7f6c72
sbonami <scott@pypline.com>
2013-12-17 23:35:36 -0500

    Add empty specs for iOS and OS X specific tests

--- Tests/OS X Test/OS_X_Test.m
+++ Tests/OS X Test/OS_X_Test.m
@@ -6,24 +6,8 @@
 //
 //
 
-#import <XCTest/XCTest.h>
+#import <Kiwi/Kiwi.h>
 
-@interface OS_X_Test : XCTestCase
+SPEC_BEGIN(OSXSpecificTests)
 
-@end
-
-@implementation OS_X_Test
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-@end
+SPEC_END
